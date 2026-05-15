b1d90870285cd4b3eb7b67912680eb4023e8ea73
Lokesh Paneru <lokeshpaneru20508@gmail.com>
2026-05-15 22:37:48 +0530

    Added Python To-Do List project

--- Task1_TodoList/readme.md
+++ Task1_TodoList/readme.md
@@ -1,8 +1,8 @@
-### Python To-Do List Application
+# Python To-Do List Application
 
 A simple command-line based To-Do List application built using Python for the CodSoft Python Programming Internship.
 
-#### Features
+## Features
 - Add new tasks
 - View all tasks
 - Delete tasks
@@ -10,18 +10,33 @@ A simple command-line based To-Do List application built using Python for the Co
 - Input validation
 - Terminal screen clearing for better user experience
 
-#### Technologies Used
+## Technologies Used
 - Python
 - File Handling
 - Exception Handling
 
-### How It Works
+## How It Works
 - Tasks are stored in a `task.txt` file
 - Tasks remain saved even after closing the program
 - Users can manage tasks using a menu-driven interface
 
-#### How to Run
+## How to Run
 
 ```bash
 python todo.py
 ```
+
+Project Structure
+```
+Task1_TodoList/
+│
+├── todo.py
+├── task.txt
+└── README.md
+```
+Internship
+
+This project was created as Task 1 for the CodSoft Python Programming Internship.
+
+
+---
--- Task1_TodoList/todo.py
+++ Task1_TodoList/todo.py
@@ -1,62 +1,63 @@
 tasks = [] #initialized list to store tasks
 # TODO List for CODSOFT Project Internship Task
 
-
-def clear_screen(a):
-  if a == True:
-    import os
-    os.system("cls||clear")
-    print("\n" * 50)
-  else:
-    pass
+# Function to clear terminal screen
+def clear_screen():
+  import os
+  os.system("cls || clear")
 
 
+# Function to load tasks from text file
 def loadFile():
   global tasks
   try:
     with open("task.txt", "r") as f:
        tasks = [x.strip() for x in f]
-  except:
-    task = open("task.txt", "w")
-    print("Task.txt file now found. Creating new file...")
+  except FileNotFoundError:
+    open("task.txt", "w").close()
+    print("Task.txt file now found. Creating new file...\nFile Created Sucessfully! You can now add tasks to your list!\n\n")
 
-
+# Function to save updated tasks into file
 def updateFile():
   with open("task.txt", "w") as f:
     for tsk in tasks:
       f.write(tsk +"\n")
 
-
+# Function to add new task
 def newTask():
   newTask = input("Enter new task: ")
   tasks.append(newTask)
 
-
+# Function to display all tasks
 def viewTask():
   if len(tasks) == 0:
     print("Task list is Empty, Try adding some tasks and try again!")
   else:
     for x in range(len(tasks)):
-      print(x+1,". ", tasks[x])
+      print(f"{x+1}. {tasks[x]}")
 
-  
+# Function to delete selected task
 def delTask():
   viewTask()
-  if len(tasks) != 0:
+  if tasks:
     try:
       a = input("Enter the Task number to Delete: ")
       inp = int(a)
-      tasks.pop(inp -1)
-      print("Task Removed! This is new Task List: \n")
-      viewTask()
-    except:
+      if 1 <= inp <=len(tasks):
+        tasks.pop(inp -1)
+        print("Task Removed! This is new Task List: \n")
+        viewTask()
+      else:
+        print("Enter valid list number.")
+    except ValueError:
       print("Enter the Numeric Value !")
   else:
     print("Nothing to Delete on the Task list!")
 
 
-
+# Main program loop
 def main():
+  clear_screen()
   loadFile()
   while True:
     print("""Select the operation you want to perform:\n
@@ -66,28 +67,33 @@ def main():
       \t4. Exit""", end='\n')
     choice = input("Enter Your choice: ")
     if choice == '1':
-      clear_screen(True)
+      clear_screen()
       newTask()
       print("Task Added!")
+      input("Press Enter to continue...")
+      clear_screen()
 
     elif choice == '2':
-      clear_screen(True)
+      clear_screen()
       print("These are your current Tasks list: \n")
       viewTask()
-      inp = input("Press Enter to go back to menu: ")
+      inp = input("Press Enter to go back to menu: \n\n")
       if inp == '':
+        clear_screen()
         pass
     elif choice == '3':
-      clear_screen(True)
+      clear_screen()
       delTask()
 
-      inp = input("Press Enter to save and go back to menu: ")
+      inp = input("Press Enter to save and go back to menu: \n\n")
       if inp == '':
+        clear_screen()
         pass
     elif choice == '4':
       break
     else:
       print("Choose Valid operation!!!")
-      clear_screen(False)
     updateFile()
+
+# Finally called main function to run the whole program :)
 main()
